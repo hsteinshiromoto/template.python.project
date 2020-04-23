e33b1e0362b393d1cc02a1a8a4b04af233fef99c
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2020-04-23 20:45:54 +1000

    Fix bugs in entrypoint

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,10 +5,10 @@ set -e
 CURRENT_UID=${uid:-9999}
 
 # If "-e docker_user={custom/local user id}" flag is not set for "docker run" command, use docker_user as default
-CONTAINER_USERNAME=${docker_user:docker_user}
+CONTAINER_USERNAME=${DOCKER_USER:-"docker_user"}
 
 # Create user called "docker" with selected UID
-useradd --shell /bin/bash -p $(openssl passwd -1 $CONTAINER_PASSWORD) -u $CURRENT_UID -o -c "" -d $PROJECT_DIR $CONTAINER_USERNAME
+useradd --shell /bin/bash -p $(openssl passwd -1 $CONTAINER_PASSWORD) -u $CURRENT_UID --no-create-home $CONTAINER_USERNAME
 
 # Start SSH Server
 service ssh start
