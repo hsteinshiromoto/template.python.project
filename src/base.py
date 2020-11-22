d1ce0b529bab7f33c54b7e84a1658a2cd3d026a9
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2020-11-23 08:43:41 +1100

    Add get_settings function

--- src/base.py
+++ src/base.py
@@ -1,6 +1,32 @@
-from pathlib import Path
 import subprocess
+from pathlib import Path
 
-def get_root_path():
-    return Path(subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], 
-                                stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8'))
+import yaml
+
+PROJECT_ROOT = Path(subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], 
+stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8'))
+
+
+def get_settings(basename: str="settings.yml", path: Path=PROJECT_ROOT / "conf") -> dict:
+    """
+    Loads settings file
+
+    Args:
+        basename (str, optional): Basename of settings file. Defaults to "settings.yml".
+        path (Path, optional): Path of seetings file. Defaults to PROJECT_ROOT/"conf".
+
+    Raises:
+        exc: Yaml load exception
+
+    Returns:
+        dict: settings
+    """
+
+    with open(str(path / basename), 'r') as stream:
+        try:
+            settings = yaml.safe_load(stream)
+
+        except yaml.YAMLError as exc:
+            raise exc
+
+    return settings
