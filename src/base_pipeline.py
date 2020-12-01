7bec397e6dec67209fc39753825da388f2b63205
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2020-12-01 13:58:25 +1100

    Add comments

--- src/base_pipeline.py
+++ src/base_pipeline.py
@@ -14,6 +14,15 @@ sys.path.append(str(PROJECT_ROOT))
 
 
 class EPipeline(Pipeline):
+    """
+    Extends Pipeline class to add the get_feature_names method
+
+    Args:
+        Pipeline (Pipeline): Sci-kit learn pipeline class
+
+    Src:
+        https://stackoverflow.com/questions/48005889/get-features-from-sklearn-feature-union
+    """
     def __init__(self, steps: list, **kwargs):
         super().__init__(steps=steps, **kwargs)
 
@@ -22,6 +31,13 @@ class EPipeline(Pipeline):
 
 
 class Extract(BaseEstimator, TransformerMixin):
+    """
+    Extracts specific columns of a data frame
+
+    Args:
+        BaseEstimator (BaseEstimator): Sci-kit learn BaseEstimator class
+        TransformerMixin (TransformerMixin): Sci-kit learn TransformerMixin class
+    """
     def __init__(self, column: list=None):
         if not isinstance(column, list):
             msg = f"Expected argument to be of type list. Got {type(column)}."
