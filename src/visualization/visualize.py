47f8d3ffc7a2d4a5ed68686f436dd4b25ae50c51
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-01-23 12:06:00 +1100

    improve time series plot

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -12,6 +12,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable, axes_size
 
 from itertools import zip_longest 
 from copy import copy
+from typeguard import typechecked
 
 
 def heatmap_4d(volume: pd.DataFrame, probabilities: pd.DataFrame
@@ -35,9 +36,7 @@ def heatmap_4d(volume: pd.DataFrame, probabilities: pd.DataFrame
         >>> ncols = 50
         >>> volume = pd.DataFrame(np.random.randint(0, 1000, size=(nrows, ncols)), columns=[f"col_{i}" for i in range(ncols)])
         >>> probabilities = pd.DataFrame(np.random.randn(nrows, ncols), columns=[f"col_{i}" for i in range(ncols)])
-        >>> heatmap, legend = heatmap_4d(volume, probabilities, xlabel="Category_1", ylabel="Category_2")
-        >>> assert(isinstance(heatmap, matplotlib))
-        True
+        >>> _, _ = heatmap_4d(volume, probabilities, xlabel="Category_1", ylabel="Category_2")
 
     References:
         [1] https://blogs.oii.ox.ac.uk/bright/2014/08/12/point-size-legends-in-matplotlib-and-basemap-plots/
@@ -161,23 +160,47 @@ def plot_target_proba(feature: str, probability_column_name: str, volume: str,
     return proba, vol
 
 
-#! Todo: Add tests. Improve code
-def plot_time_series(feature: str, datetime_column_name: str, volume: str, 
-                    data: pd.DataFrame):
+@typechecked
+def plot_time_series(feature: str, datetime_column_name: str, 
+                    data: pd.DataFrame, volume: str=None):
+    #! Add tests
+    """Plots a time series of a given feature
+
+    Args:
+        feature (str): Feature to be plotted
+        datetime_column_name (str): Name of the datetime column used to plot
+        data (pd.DataFrame): Dataframe contaning the features to be plotted
+        volume (str, optional): A second feature to be plotted in a bar plot
+
+    Returns:
+        [matplotlib]: time series and barplot
+    """
 
     # Instantiate plotting objects
-    fig, (proba, vol) = plt.subplots(nrows=2)
+    if volume:
+        fig, (proba, vol) = plt.subplots(nrows=2)
+
+    else:
+        fig, proba = plt.subplots()
 
     # Plotting probabilities
     proba.plot(data[datetime_column_name], data[feature], linestyle="-", marker="o")
-    proba.set_title(f"{feature}")
+    proba.set_title(f"Time Series Plot of {feature}")
     proba.set_ylabel(feature)
-    proba.set_xticklabels([])
+    
+    if volume:
+        proba.set_xticklabels([])
 
-    # Plotting volumes
-    vol = sns.barplot(data[datetime_column_name], data[volume].values)
-    vol.set_xlabel(datetime_column_name)
-    x_dates = data[datetime_column_name].dt.strftime('%Y-%m-%d').sort_values()
-    vol.set_xticklabels(labels=x_dates, rotation=45)
+        # Plotting volumes
+        vol = sns.barplot(data[datetime_column_name], data[volume].values)
+        vol.set_xlabel(datetime_column_name)
+        vol.set_ylabel("Volume")
+        x_dates = data[datetime_column_name].dt.strftime('%Y-%m-%d').sort_values()
+        vol.set_xticklabels(labels=x_dates, rotation=45)
 
-    return proba, vol
+        return proba, vol
+
+    else:
+        proba.set_xlabel(datetime_column_name)
+
+        return proba
