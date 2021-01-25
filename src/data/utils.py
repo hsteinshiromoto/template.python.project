0bb29f515182966c9f8042f083f27f0a42a22885
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-01-25 15:56:36 +1100

    Bugfix

--- src/data/utils.py
+++ src/data/utils.py
@@ -18,23 +18,24 @@ from tests.mock_dataset import mock_dataset
 
 
 @typechecked
-def bin_and_agg(feature: str, bin_feature: str, data: pd.DataFrame
+def bin_and_agg(feature: str, data: pd.DataFrame, secondary_feature: str=None
                 ,bins: Union[np.array, str]=None, func: str="count"):
     """Aggregate feature according to bins. Use to Freedman-Diaconis Estimator 
     calculate bins [1].
 
     Args:
-        feature (str): Feature to be aggregated
-        bin_feature (str): Feature that will be binarized
+        feature (str): Feature binarized and aggregated, if a secondary_feature is not passed
         data (pd.DataFrame): Dataframe containing both features
+        secondary_feature (str): Feature that is aggregated
         bins (np.array or str, optional): Array containing the bins. Defaults to None.
         func (str, optional): Function used to aggregate. Defaults to "count".
 
     Returns:
         pd.DataFrame: binarized and aggregated data
 
-    Src:
-        [1] https://stats.stackexchange.com/questions/798/calculating-optimal-number-of-bins-in-a-histogram
+    References:
+        [1] https://stats.stackexchange.com/questions/798/
+            calculating-optimal-number-of-bins-in-a-histogram
 
     Example:
         >>> specs = {"float": [100, 1, 0] \
@@ -45,17 +46,24 @@ def bin_and_agg(feature: str, bin_feature: str, data: pd.DataFrame
                         ,"datetime": [100, 1, 0] \
                         }
         >>> data, meta_data = mock_dataset(specs=specs, meta_data=True)
-        >>> df = bin_and_agg("int", "float", data)
+        >>> df = bin_and_agg(feature="float_0", data=data)
+        >>> "count_float_0" in df.columns.values
+        True
+        >>> df = bin_and_agg(feature="float_0", secondary_feature="int_0", data=data)
+        >>> ("count_int_0" in df.columns.values) and ("count_float_0" not in df.columns.values)
+        True
     """
+    
+    bins = bins or np.histogram_bin_edges(data[feature].values, bins="fd")
 
-    if not bins:
-        bins = np.histogram_bin_edges(data[bin_feature].values, bins="fd")
+    secondary_feature = secondary_feature or feature
 
-    return_dict = {"count": data.groupby(pd.cut(data[bin_feature], bins=bins))[feature].count()
-                    ,"sum": data.groupby(pd.cut(data[bin_feature], bins=bins))[feature].sum()
-                    ,"mean": data.groupby(pd.cut(data[bin_feature], bins=bins))[feature].mean()
-                    ,"min": data.groupby(pd.cut(data[bin_feature], bins=bins))[feature].mean()
-                    ,"max": data.groupby(pd.cut(data[bin_feature], bins=bins))[feature].mean()
+    return_dict = {"count": data.groupby(pd.cut(data[feature], bins=bins))[secondary_feature].count()
+                    ,"sum": data.groupby(pd.cut(data[feature], bins=bins))[secondary_feature].sum()
+                    ,"mean": data.groupby(pd.cut(data[feature], bins=bins))[secondary_feature].mean()
+                    ,"min": data.groupby(pd.cut(data[feature], bins=bins))[secondary_feature].min()
+                    ,"max": data.groupby(pd.cut(data[feature], bins=bins))[secondary_feature].max()
                     }
 
-    return return_dict[func].reset_index().to_frame(name=func)
+    return return_dict[func].to_frame(name=f"{func}_{secondary_feature}").\
+            reset_index().rename(columns={feature: "bins"})
