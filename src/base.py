fe6c264d56c15fc80c5cc86f390a7d74148176ed
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-05-12 15:22:29 +1000

    improve class

--- src/base.py
+++ src/base.py
@@ -10,45 +10,38 @@ PROJECT_ROOT = Path(subprocess.Popen(['git', 'rev-parse', '--show-toplevel'],
 stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8'))
 
 class Get_Filename(object):
-    @abstractmethod
-    def __init__(self, path: Path, basename: Union[str, Path]=None
-                ,pattern: str="*"):
+    def __init__(self, path: Path):
         self.path = path
-        self.basename = Path(basename) if basename else None
-        self.pattern = pattern
 
-    @abstractmethod
-    def list_files(self, sort_new: bool=False, sort_alphabet: bool=False):
-        output = list(filter(Path.is_file, self.path.glob(self.pattern)))
 
-        if sort_new:
-            return sorted(output, key=os.path.getctime, reverse=True)
+    def list_files(self, pattern: str="*", sort: bool=False):
+        files_list = list(filter(Path.is_file, self.path.glob(pattern)))
 
-        if sort_alphabet:
-            return sorted(output)
+        output = sorted(files_list, key=os.path.getctime, reverse=True)
 
-        return output
+        if sort:
+            output = sorted(files_list)
 
-    @abstractmethod
-    def load(self):
-        with open(str(self.path / self.basename), 'rb') as file:
-            if self.basename.suffix in ["yml", "yaml"]:
+        yield from output
+
+
+    def load(self, basename: Union[str, Path]=None):
+        with open(str(self.path / basename), 'rb') as file:
+            if basename.suffix in ["yml", "yaml"]:
                 return yaml.safe_load(file)
 
             else:
                 return file.open()
 
-    @abstractmethod
-    def newest(self):
-        return self.list_files(sort_new=True)[0]
 
-    @abstractmethod
-    def __iter__(self):
-        yield from self.list_files()
+    def load_newest(self, pattern: str="*"):
+        newest = self.newest(pattern=pattern)
+        return self.load(newest.name)
 
-    @abstractmethod
-    def __next__(self): # Python 2: def next(self)
-        return self
+
+    def newest(self, pattern: str="*"):
+        return self.list_files(pattern=pattern)[0]
+
 
 
 
@@ -78,16 +71,10 @@ def get_settings(basename: str="settings.yml"
 
 
 if __name__ == "__main__":
-    get_file = Get_File(path=Path(__file__).resolve().parent)
+    get_file = Get_Filename(path=Path(__file__).resolve().parent)
 
-    for item in get_file:
-        print(item)
-
-    print(f"Newest: {get_file.newest()}\n")
-
-    print(f"Sort by date: {get_file.list_files(sort_new=True)}\n")
-
-    print(f"Sort alphabetically: {get_file.list_files(sort_alphabet=True)}\n")
+    for item in get_file.list_files(pattern="*.py", sort=True):
+        print(type(item))
 
     # for item in filter(Path.is_file, Path(__file__).resolve().parent.glob("*")):
     #     print(item)
