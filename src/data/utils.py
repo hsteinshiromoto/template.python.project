68bd26c0ad3216f9f23c4e0229ebee2ee02a0331
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-06-02 20:35:15 +1000

    improve function

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+from collections.abc import Iterable
 from pathlib import Path
 from typing import Union
 
@@ -146,34 +147,58 @@ def make_pivot(feature: str, index: str, column: str, data: pd.DataFrame
 
 @log_fun
 @typechecked
-def truncate_prop(column: str, data: pd.DataFrame, prop_threshold: float=0.8):
+def get_high_frequency_categories(array: Iterable
+                                ,method: dict={"pareto": (0.2, 0.8)
+                                            ,"top": 10
+                                            }
+                                ,return_summary: bool=True):
     """Truncates data according to the proportion of a categorical column
 
     Args:
-        column (str): Name of categorical column
-        data (pd.DataFrame): Data frame
-        prop_threshold (float, optional): Proportion of cumulative proportion above which the data frame will the truncated. Defaults to 0.8.
+        array (Iterable): 1d array containing categories
+        method (dict): Get high frequency using method
 
     Returns:
-        (pd.DataFrame): Truncated data frame
+        (list, pd.DataFrame): Truncated list and summary statistics
 
     References:
         [1] https://hsteinshiromoto.github.io/posts/2020/06/25/find_row_closest_value_to_input
     """
-    # Group data
-    grouped = data[column].value_counts(normalize=True).to_frame(name="Proportions")
-    grouped["Cumulative_Proportions"] = grouped["Proportions"].cumsum()
-    grouped["Count"] = data[column].value_counts()
+    unique, counts = np.unique(array, return_counts=True)
+    grouped = pd.DataFrame.from_dict(dict(zip(unique, counts))
+                                    ,columns=["category", "n_observations"])
+    grouped["n_observations_proportions"] = grouped["n_observations"] / grouped["n_observations"].sum()
+    grouped["cum_n_observations_proportions"] = grouped["n_observations_proportions"].cumsum()
     grouped.reset_index(inplace=True)
-    grouped.rename(columns={"index": column}, inplace=True)
+    grouped.rename(columns={"index": "categories_count"}, inplace=True)
+    grouped["categories_proportion"] = grouped["categories_count"] / grouped["categories_count"].max()
+    grouped["cum_categories_proportion"] = grouped["categories_proportion"].cumsum()
 
-    # Get row containing values closed to a value [1]
-    idx = grouped["Cumulative_Proportions"].sub(prop_threshold).abs().idxmin()
-    grouped.loc[idx, column] = "Other Values"
-    grouped.loc[idx, "Cumulative_Proportions"] = 1
+    if "pareto" in method:
+        subset = grouped["cum_n_observations_proportions"] + grouped["cum_categories_proportion"]
+        threshold = np.sum(method.values())
+        # Get row containing values closed to a value [1]
+        idx = subset.sub(threshold).abs().idxmin()
 
-    mask = grouped["Cumulative_Proportions"]  > prop_threshold
-    grouped.loc[idx, "Proportions"] = grouped.loc[mask, "Proportions"].sum()
-    grouped.loc[idx, "Count"] = grouped.loc[mask, "Count"].sum()
+    elif "top" in method:
+        idx = method.values()
 
-    return grouped.iloc[:idx+1, :]
+    else:
+        msg = f"Expected method to be pareto or top. Got {list(method.keys())}."
+        raise NotImplementedError(msg)
+
+    grouped.loc[idx, "category"] = "other categories"
+    grouped.loc[idx, "cum_n_observations_proportions"] = 1
+    grouped.loc[idx, "cum_categories_proportion"] = 1
+
+    grouped.loc[idx, "n_observations"] = grouped.loc[idx:, "n_observations"].sum()
+    grouped.loc[idx, "n_observations_proportions"] = grouped.loc[idx:, "n_observations_proportions"].sum()
+    grouped.loc[idx, "categories_count"] = grouped.loc[idx:, "categories_count"].sum()
+    grouped.loc[idx, "categories_proportion"] = grouped.loc[idx:, "categories_proportion"].sum()
+
+    output = [item for item in array if item in grouped.loc[:idx, "category"].values]
+
+    if not return_summary:
+        return output
+
+    return output, grouped.loc[:idx+1, :]
